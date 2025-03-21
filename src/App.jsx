5f281e17d7fdc6334b5234a3af3df523ba5975d6
LONTSIPARFAIT <lontsiparfait12@gmail.com>
2025-03-21 15:20:23 +0100

    first commit

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,7 @@ import React from "react";
 const App = () => {
   return (
     <div className="w-20 h-10 bg-neutral-500 ">
-      <h1 class="text-3xl font-bold underline"></h1>
+      
     </div>
   );
 };
