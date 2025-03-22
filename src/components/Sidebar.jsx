b36b776cbe7949e7b54f83a200798954ba51d431
LONTSIPARFAIT <lontsiparfait12@gmail.com>
2025-03-22 09:43:03 +0100

    first commit

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -13,7 +13,7 @@ const Sidebar = () => {
             <img className='w-6' src={assets.search_icon} alt="" />
             <div className="font-bold">Search</div>
         </div>
-        <div className="bg-[#121212] h-[85%] rounded flex flex-col justify-around">
+        <div className="bg-[#121212] h-[85%] rounded">
 
         </div>
       </div>
