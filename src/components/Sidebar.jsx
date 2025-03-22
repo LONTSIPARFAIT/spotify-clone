07960fd21e1328bcbb798307729f08dee70ec7bf
LONTSIPARFAIT <lontsiparfait12@gmail.com>
2025-03-22 09:11:33 +0100

    first commit

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -3,8 +3,8 @@ import React from 'react'
 const Sidebar = () => {
   return (
     <div className='w-[25%] h-full p-2 flex-col gap-2 hidden lg:flex bg-white'>
-      <div className="bg-[#121212] h-[15%] rounded flex flex-col">
-        
+      <div className="bg-[#121212] h-[15%] rounded flex flex-col justify-around">
+        <div className=""></div>
       </div>
     </div>
   )
