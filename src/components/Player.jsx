4701ff9eb34ff23e051708d0f83d9c8e3f63ec64
LONTSIPARFAIT <lontsiparfait12@gmail.com>
2025-03-22 20:15:57 +0100

    first commit

--- src/components/Player.jsx
+++ src/components/Player.jsx
@@ -42,8 +42,9 @@ const Player = () => {
         <div className="flex items-center gap-5">
           <p>1:06</p>
           <div className="w-[60vw] max-w-[500px] bg-gray-300 rounded-full cursor-pointer">
-            <hr className='h-1 border-none' />
+            <hr className='h-1 border-none w-0 bg-green-800 rounded-full' />
           </div>
+          <p></p>
         </div>
       </div>
     </div>
