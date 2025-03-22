033322da244c5680c51ccb79fa1a854b3b59ed66
LONTSIPARFAIT <lontsiparfait12@gmail.com>
2025-03-22 19:58:58 +0100

    first commit

--- src/components/Player.jsx
+++ src/components/Player.jsx
@@ -11,7 +11,7 @@ const Player = () => {
             <p> {songsData[0].desc.slice(0,12)} </p>
         </div>
       </div>
-      <div className="flex flex-col"></div>
+      <div className="flex flex-col items-center"></div>
     </div>
   )
 }
