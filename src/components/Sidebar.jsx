27537fe70e65d7ca3e67329807c755e95f8b2dea
LONTSIPARFAIT <lontsiparfait12@gmail.com>
2025-03-22 11:06:23 +0100

    first commit

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -32,7 +32,7 @@ const Sidebar = () => {
                 <button className='px-4 py-1.5 bg-white text-[15px] text-black rounded-full mt-4 cursor-pointer'>Creer La PlayList</button>
             </div>
             <div className="p-4 bg-[#242424] m-2 rounded font-semibold flex flex-col items-start justify-start gap-1 pl-4">
-                <h1>Créez votre première playlist</h1>
+                <h1>Let's findsome podcasts to follow</h1>
                 <p className='font-light'>c'est facile, nous vous aiderons</p>
                 <button className='px-4 py-1.5 bg-white text-[15px] text-black rounded-full mt-4 cursor-pointer'>Creer La PlayList</button>
             </div>
