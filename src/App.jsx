427b84206537e0b9154b3bdb1c941891c3756a2f
LONTSIPARFAIT <lontsiparfait12@gmail.com>
2025-03-22 08:58:19 +0100

    first commit

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,9 @@ import React from "react";
 const App = () => {
   return (
     <div className="h-screen bg-black">
-      <div className=""></div>
+      <div className="">
+        
+      </div>
     </div>
   );
 };
