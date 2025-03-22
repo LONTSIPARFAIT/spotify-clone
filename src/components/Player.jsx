88cc9ed75ed0ff47125bc837f0644c523f6e13ce
LONTSIPARFAIT <lontsiparfait12@gmail.com>
2025-03-22 19:47:17 +0100

    first commit

--- src/components/Player.jsx
+++ src/components/Player.jsx
@@ -6,6 +6,7 @@ const Player = () => {
     <div className='h-[10%] bg-black flex justify-between items-center text-white px-4'>
       <div className="hidden lg:flex items-center gap-4">
         <img src={songsData[0].image} alt="" />
+        <div className=""></div>
       </div>
     </div>
   )
