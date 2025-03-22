28138d55b7cfaf0bd5d46714ced6043e8be198c0
LONTSIPARFAIT <lontsiparfait12@gmail.com>
2025-03-22 09:34:24 +0100

    first commit

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -3,11 +3,11 @@ import {assets} from '../assets/assets'
 
 const Sidebar = () => {
   return (
-    <div className='w-[25%] h-full p-2 flex-col gap-2 hidden lg:flex bg-white'>
+    <div className='w-[25%] h-full p-2 flex-col gap-2 hidden lg:flex text-white'>
       <div className="bg-[#121212] h-[15%] rounded flex flex-col justify-around">
         <div className="flex items-center gap-3 pl-8 cursor-pointer">
             <img src={assets.home_icon} alt="" />
-            <div className="text-white font-semibold">Home</div>
+            <div className="font-semibold">Home</div>
         </div>
       </div>
     </div>
