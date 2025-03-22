eba4f611d2d7041a496ea73e319855d8cc91ec0f
LONTSIPARFAIT <lontsiparfait12@gmail.com>
2025-03-22 09:47:28 +0100

    first commit

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -16,7 +16,8 @@ const Sidebar = () => {
         <div className="bg-[#121212] h-[85%] rounded">
             <div className="p-4 flex items-center justify-between">
                 <div className="flex items-center gap-3">
-                    <img className='' src={assets.stack_icon} alt="" />
+                    <img className='w-8' src={assets.stack_icon} alt="" />
+                    <div className="">Votre Bibliothèque</div>
                 </div>
             </div>
         </div>
