bb1e24a25812de1df962632198f30851294d04b2
LONTSIPARFAIT <lontsiparfait12@gmail.com>
2025-03-22 09:05:39 +0100

    first commit

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 const Sidebar = () => {
   return (
-    <div className='w-[25%] h-full p-2 bg-white'>
+    <div className='w-[25%] h-full p-2 flex-col bg-white'>
       Sidebar
     </div>
   )
