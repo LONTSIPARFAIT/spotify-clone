d126d41fbf064ba245862c70fdc98167e57256ac
LONTSIPARFAIT <lontsiparfait12@gmail.com>
2025-03-22 09:59:28 +0100

    first commit

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -27,8 +27,8 @@ const Sidebar = () => {
             <div className="p-4 bg-[#242424] m-2 rounded font-semibold flex flex-col items-start justify-start gap-1 pl-4">
                 <h1>Créez votre première playlist</h1>
                 {/* <h1>Create your first playlist</h1> */}
-                <p>it's easy we will help you</p>
-                <p>it's easy we will help you</p>
+                <p>c'est facile, nous vous aiderons</p>
+                {/* <p>it's easy we will help you</p> */}
             </div>
         </div>
       </div>
