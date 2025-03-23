c9b4162eba44ae6823e8c1bca8957f3e8507268b
LONTSIPARFAIT <lontsiparfait12@gmail.com>
2025-03-23 20:49:39 +0100

    first commit

--- src/components/Display.jsx
+++ src/components/Display.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 const Display = () => {
   return (
-    <div className='w-[100%] m-2 px-6 pt-4 rounded bg-[]'>
+    <div className='w-[100%] m-2 px-6 pt-4 rounded bg-[#]'>
       Display
     </div>
   )
