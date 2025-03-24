3b5a682ec0c959236a1ba1c478807e582948bee6
LONTSIPARFAIT <lontsiparfait12@gmail.com>
2025-03-24 20:17:19 +0100

    spotify

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -4,8 +4,8 @@ const Navbar = () => {
   return (
     <>
       <div className="w-full flex justify-between items-center font-semibold">
-        <div className="flex items-center">
-
+        <div className="flex items-center gap-2">
+          <img src="" alt="" />
         </div>
       </div>
     </>
