e029ada3b39a8fbaeaf6ef0cd98f30769e23ff0f
LONTSIPARFAIT <lontsiparfait12@gmail.com>
2025-03-24 21:00:02 +0100

    spotify

--- src/components/DisplayHome.jsx
+++ src/components/DisplayHome.jsx
@@ -1,12 +1,13 @@
 import React from 'react'
 import Navbar from './Navbar'
+import { albumsData } from '../assets/assets'
 
 const DisplayHome = () => {
   return (
     <>
       <Navbar />
       <div className="my-5 font-bold text-2xl">
-        d
+        {albumsData}
       </div>
     </>
   )
