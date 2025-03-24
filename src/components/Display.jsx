2eb6afa022545fdbf15308660c0bd2a83ecafdb4
LONTSIPARFAIT <lontsiparfait12@gmail.com>
2025-03-24 20:06:38 +0100

    spotify

--- src/components/Display.jsx
+++ src/components/Display.jsx
@@ -1,9 +1,12 @@
 import React from 'react'
+import { Route, Routes } from 'react-router-dom'
 
 const Display = () => {
   return (
     <div className='w-[100%] m-2 px-6 pt-4 rounded bg-[#121212] text-white overflow-auto lg:w-[75%] lg:ml-0'>
-      Display
+      <Routes>
+        <Route />
+      </Routes>
     </div>
   )
 }
