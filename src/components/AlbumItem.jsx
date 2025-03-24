4f3a4e7f54b3441788a2851936b3d2e61dd25028
LONTSIPARFAIT <lontsiparfait12@gmail.com>
2025-03-24 20:52:48 +0100

    spotify

--- src/components/AlbumItem.jsx
+++ src/components/AlbumItem.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 const AlbumItem = (image,name,desc,id) => {
   return (
-    <div className='min-w-[180px] p-2 px-3 rounded'>
+    <div className='min-w-[180px] p-2 px-3 rounded cursor-pointer'>
       AlbumItem
     </div>
   )
