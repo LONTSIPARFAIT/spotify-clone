d58c49fb3f0d3f1413bf02557b2555246fc7e67a
LONTSIPARFAIT <lontsiparfait12@gmail.com>
2025-03-24 20:55:48 +0100

    spotify

--- src/components/AlbumItem.jsx
+++ src/components/AlbumItem.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 const AlbumItem = (image,name,desc,id) => {
   return (
     <div className='min-w-[180px] p-2 px-3 rounded cursor-pointer hover:bg-[#ffffff26]'>
-      <img src={""} alt="" />
+      <img src={image} alt="" />
     </div>
   )
 }
