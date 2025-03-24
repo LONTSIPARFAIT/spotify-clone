7bc293d06509bc00238d2ff12de989dcb6c7a544
LONTSIPARFAIT <lontsiparfait12@gmail.com>
2025-03-24 21:18:58 +0100

    spotify

--- src/components/DisplayHome.jsx
+++ src/components/DisplayHome.jsx
@@ -10,7 +10,7 @@ const DisplayHome = () => {
       <div className="mb-5">
         <h1 className="my-5 text-slate-200 font-bold text-2xl">Featured Charts</h1>  
         <div className="flex overflow-auto">
-          {albumsData.map((item,index)=>(<AlbumItem key={index} name={item.name} desc={item.desc} id={item.id} image={item.image} />))}
+          {albumsData.map((item,index)=>(<AlbumItem key={index} image={item.image} name={item.name} desc={item.desc} id={item.id}  />))}
 
         </div>
       </div>
