6833eab1e8dd4e2a99662dc9982b7f600872f171
LONTSIPARFAIT <lontsiparfait12@gmail.com>
2025-03-24 20:25:25 +0100

    spotify

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -10,7 +10,7 @@ const Navbar = () => {
           <img className='w-8 bg-black p-2 rounded-2xl cursor-pointer' src={assets.arrow_right} alt="" />
         </div>
         <div className="flex items-center gap-4">
-          <p className='bg-white'>Explore Premium</p>
+          <p className='bg-white text-black texy-'>Explore Premium</p>
         </div>
 
       </div>
