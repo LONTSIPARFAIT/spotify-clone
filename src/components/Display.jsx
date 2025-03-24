b2e1dca19f75ad0957c066d8f6e2f9d6eda6dd0a
LONTSIPARFAIT <lontsiparfait12@gmail.com>
2025-03-24 20:07:51 +0100

    spotify

--- src/components/Display.jsx
+++ src/components/Display.jsx
@@ -5,7 +5,7 @@ const Display = () => {
   return (
     <div className='w-[100%] m-2 px-6 pt-4 rounded bg-[#121212] text-white overflow-auto lg:w-[75%] lg:ml-0'>
       <Routes>
-        <Route path=''/>
+        <Route path='/'/>
       </Routes>
     </div>
   )
