f03d816583ae8c829542b6c4f83fc24d889c53bb
LONTSIPARFAIT <lontsiparfait12@gmail.com>
2025-03-24 20:57:33 +0100

    spotify

--- src/components/AlbumItem.jsx
+++ src/components/AlbumItem.jsx
@@ -4,6 +4,7 @@ const AlbumItem = (image,name,desc,id) => {
   return (
     <div className='min-w-[180px] p-2 px-3 rounded cursor-pointer hover:bg-[#ffffff26]'>
       <img className='rounded' src={image} alt="" />
+      <p className="font-bold"> {name} </p>
     </div>
   )
 }
