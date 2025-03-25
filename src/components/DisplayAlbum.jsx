4565a7ab3c9ef16db8ba986d27558a8fccf25233
LONTSIPARFAIT <lontsiparfait12@gmail.com>
2025-03-25 10:04:46 +0100

    spotify

--- src/components/DisplayAlbum.jsx
+++ src/components/DisplayAlbum.jsx
@@ -42,7 +42,7 @@ const DisplayAlbum = () => {
           <div key={index} className="grid grid-cols-3 sm:grid-cols-4 gap-2 p-2 items-center text-[#a7a7a7] hover:text-[#ffffff2b] cursor-pointer">
              <p className="text-white">
               <b className='mr-4 text-[#a7a7a7]'> {index + 1} </b>
-              <img src="" alt="" />
+              <img src={item.image} alt="" />
              </p>
           </div>
         })
