f672a23218e54933156864cf52a86e4053b8ca6f
LONTSIPARFAIT <lontsiparfait12@gmail.com>
2025-03-25 08:53:57 +0100

    spotify

--- src/components/DisplayHome.jsx
+++ src/components/DisplayHome.jsx
@@ -9,7 +9,7 @@ const DisplayHome = () => {
       <Navbar />
       <div className="mb-5">
         <h1 className="my-5 text-slate-200 font-bold text-2xl">Featured Charts</h1>  
-        <div className="">
+        <div className="flex overflow-auto">
           {albumsData.map((item,index)=>(<AlbumItem key={index} image={item.image} name={item.name} desc={item.desc} id={item.id} />))}
           <p>Test</p>
           <p>Test 2</p>
