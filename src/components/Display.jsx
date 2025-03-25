bd3bde94ded634e5df16a25bc950b0c8ae1dbe84
LONTSIPARFAIT <lontsiparfait12@gmail.com>
2025-03-25 11:02:02 +0100

    spotify

--- src/components/Display.jsx
+++ src/components/Display.jsx
@@ -6,6 +6,7 @@ import DisplayAlbum from './DisplayAlbum'
 const Display = () => {
 
   const displayRef = useRef()
+  const location
 
   return (
     <div ref={displayRef} className='w-[100%] m-2 px-6 pt-4 rounded bg-[#121212] text-white overflow-auto lg:w-[75%] lg:ml-0'>
