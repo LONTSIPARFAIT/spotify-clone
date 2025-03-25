912c64c8df825154069f9a7d93bbd7d0e73adba6
LONTSIPARFAIT <lontsiparfait12@gmail.com>
2025-03-25 10:59:08 +0100

    spotify

--- src/components/Display.jsx
+++ src/components/Display.jsx
@@ -4,6 +4,9 @@ import DisplayHome from './DisplayHome'
 import DisplayAlbum from './DisplayAlbum'
 
 const Display = () => {
+
+  const
+
   return (
     <div className='w-[100%] m-2 px-6 pt-4 rounded bg-[#121212] text-white overflow-auto lg:w-[75%] lg:ml-0'>
       <Routes>
