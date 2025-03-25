29bd836471b8348d4fbc9c77f89fa7dcb68f339e
LONTSIPARFAIT <lontsiparfait12@gmail.com>
2025-03-25 10:59:26 +0100

    spotify

--- src/components/Display.jsx
+++ src/components/Display.jsx
@@ -1,11 +1,11 @@
-import React from 'react'
+import React, { useRef } from 'react'
 import { Route, Routes } from 'react-router-dom'
 import DisplayHome from './DisplayHome'
 import DisplayAlbum from './DisplayAlbum'
 
 const Display = () => {
 
-  const
+  const displayRef = useRef()
 
   return (
     <div className='w-[100%] m-2 px-6 pt-4 rounded bg-[#121212] text-white overflow-auto lg:w-[75%] lg:ml-0'>
