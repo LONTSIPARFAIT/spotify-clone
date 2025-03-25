856bd45c1322337ff5389f75c635f3f3081840b2
LONTSIPARFAIT <lontsiparfait12@gmail.com>
2025-03-25 09:27:57 +0100

    spotify

--- src/components/DisplayAlbum.jsx
+++ src/components/DisplayAlbum.jsx
@@ -1,11 +1,14 @@
 import React from 'react'
 import Navbar from './Navbar'
 import { useParams } from 'react-router-dom'
+import { albumsData } from '../assets/assets'
 
 const DisplayAlbum = () => {
 
   const {id} = useParams()
   // console.log(id);
+
+  const albumData = albumsData.find(item=>item.id === id)
   
 
   return (
