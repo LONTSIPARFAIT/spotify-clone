4a4f00a3042f7b42b57ce723022762d3836bad01
LONTSIPARFAIT <lontsiparfait12@gmail.com>
2025-03-25 09:50:51 +0100

    spotify

--- src/components/DisplayAlbum.jsx
+++ src/components/DisplayAlbum.jsx
@@ -31,7 +31,7 @@ const DisplayAlbum = () => {
         </div>
       </div>
       <div className="grid grid-cols-3 sm:grid-cols-4 mt-10 mb-4 pl-2 text-[#a7a7a7] gap-4">
-        <p className="text-lg font-bold"><b className='mr-4'>#</b></p>
+        <p className="text-lg font-bold"><b className='mr-4'>#</b>Titre</p>
       </div>
     </>
   )
