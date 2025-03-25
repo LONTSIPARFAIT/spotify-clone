cd3ebf1ccd128de6f050b626e596224a1aa92976
LONTSIPARFAIT <lontsiparfait12@gmail.com>
2025-03-25 09:56:08 +0100

    spotify

--- src/components/DisplayAlbum.jsx
+++ src/components/DisplayAlbum.jsx
@@ -31,9 +31,9 @@ const DisplayAlbum = () => {
         </div>
       </div>
       <div className="grid grid-cols-3 sm:grid-cols-4 mt-10 mb-4 pl-2 text-[#a7a7a7] gap-4">
-        <p className="text-lg font-bold"><b className='mr-4'>#</b>Titre</p>
-        <p className="text-lg font-bold">Album</p>
-        <p className="hidden sm:block text-lg font-bold">Date d'Ajout</p>
+        <p className=""><b className='mr-4'>#</b>Titre</p>
+        <p className="">Album</p>
+        <p className="hidden sm:block ">Date d'Ajout</p>
         <img className='m-auto w-4' src={assets.clock_icon} alt="" />
       </div>
     </>
