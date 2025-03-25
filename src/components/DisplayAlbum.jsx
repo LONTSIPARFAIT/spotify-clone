09d6d1e1c3a55541204cff2346cc33957ef5e156
LONTSIPARFAIT <lontsiparfait12@gmail.com>
2025-03-25 10:47:21 +0100

    spotify

--- src/components/DisplayAlbum.jsx
+++ src/components/DisplayAlbum.jsx
@@ -53,7 +53,7 @@ const DisplayAlbum = () => {
               <img className="inline w-10 mr-5" src={item.image} alt="" />
               {item.name}
             </p>
-            <p className=""></p>
+            <p className="">{albumData.name}</p>
           </div>
         );
       })}
