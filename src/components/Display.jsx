4e5fc53fbfa1c2a05bd752da640c793049b386a1
LONTSIPARFAIT <lontsiparfait12@gmail.com>
2025-03-25 11:13:35 +0100

    spotify

--- src/components/Display.jsx
+++ src/components/Display.jsx
@@ -13,7 +13,7 @@ const Display = () => {
   // console.log(isAlbum);
   const albumId = isAlbum ? location.pathname.slice(-1) : ""
   // console.log(albumId);
-  const bgColor = albumsData[Number]
+  const bgColor = albumsData[Number(albumId)]
   
   
   
