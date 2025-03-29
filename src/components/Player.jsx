89bbea4d6f02659f9631f1e0ec2e020558ab1f1f
LONTSIPARFAIT <lontsiparfait12@gmail.com>
2025-03-29 20:50:42 +0100

    spotify

--- src/components/Player.jsx
+++ src/components/Player.jsx
@@ -2,6 +2,9 @@ import React from "react";
 import { assets, songsData } from "../assets/assets";
 
 const Player = () => {
+
+  
+
   return (
     <div className="h-[10%] bg-black flex justify-between items-center text-white px-4">
       <div className="hidden lg:flex items-center gap-4">
