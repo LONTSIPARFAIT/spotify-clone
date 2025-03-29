b5727d0e1040def72ea0c246f10fd58497995460
LONTSIPARFAIT <lontsiparfait12@gmail.com>
2025-03-29 20:40:16 +0100

    spotify

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,9 @@ import Player from "./components/Player";
 import Display from "./components/Display";
 
 const App = () => {
+
+  
+
   return (
     <div className="h-screen bg-black">
       <div className="h-[90%] flex">
