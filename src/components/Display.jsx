e586c4af6d92ef48f89862ee96e1c31a9ade0275
LONTSIPARFAIT <lontsiparfait12@gmail.com>
2025-03-29 19:53:05 +0100

    spotify

--- src/components/Display.jsx
+++ src/components/Display.jsx
@@ -22,7 +22,7 @@ const Display = () => {
       displayRef.current.style.background = `linear-gradient(${bgColor},#121212)`
     }
     else{
-      displayRef.current.style.backgroundColor = `#121212`
+      displayRef.current.style.background = `#121212`
     }
   })
 
