ba065401036de8725bbfd1b75c40633be0d26f64
LONTSIPARFAIT <lontsiparfait12@gmail.com>
2025-03-29 20:51:46 +0100

    spotify

--- src/components/Player.jsx
+++ src/components/Player.jsx
@@ -3,7 +3,7 @@ import { assets, songsData } from "../assets/assets";
 
 const Player = () => {
 
-  const 
+  const { seekBg,seekBar }
 
   return (
     <div className="h-[10%] bg-black flex justify-between items-center text-white px-4">
