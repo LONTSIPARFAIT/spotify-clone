417a40fca7dd2e005e6bf739c8c9259d1fc654d7
LONTSIPARFAIT <lontsiparfait12@gmail.com>
2025-03-29 21:16:05 +0100

    spotify

--- src/context/PlayerContext.jsx
+++ src/context/PlayerContext.jsx
@@ -4,37 +4,34 @@ import { songsData } from "../assets/assets";
 export const PlayerContext = createContext();
 
 const PlayerContextProvider = (props) => {
+  const audioRef = useRef();
+  const seekBg = useRef();
+  const seekBar = useRef();
 
-  const audioRef = useRef()
-  const seekBg = useRef()
-  const seekBar = useRef()
-
-  const [track,setTrack] = useState(songsData[0])
-  const [playStatus,setPlayStatus] = useState(false)
-  const [time,setTime] = useState({
-    currentTime:{
-      second:0,
-      minute:0
+  const [track, setTrack] = useState(songsData[0]);
+  const [playStatus, setPlayStatus] = useState(false);
+  const [time, setTime] = useState({
+    currentTime: {
+      second: 0,
+      minute: 0,
     },
-    totalTime:{
-      second:0,
-      minute:0 
-    }
-  })
+    totalTime: {
+      second: 0,
+      minute: 0,
+    },
+  });
 
   const contextValue = {
     audioRef,
     seekBg,
     seekBar,
-  }
+  };
 
   return (
     <PlayerContext.Provider value={contextValue}>
       {props.children}
     </PlayerContext.Provider>
-  )
-
-}
-
+  );
+};
 
 export default PlayerContextProvider;
