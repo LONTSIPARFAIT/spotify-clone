f180bd48ba01270330e585c11181f655cf4853f9
LONTSIPARFAIT <lontsiparfait12@gmail.com>
2025-03-30 16:52:40 +0100

    spotify

--- src/components/SongItem.jsx
+++ src/components/SongItem.jsx
@@ -1,8 +1,9 @@
 import React, { useContext } from "react";
+import { PlayerContext } from "../context/PlayerContext";
 
 const SongItem = ({ image, name, desc, id }) => {
 
-  const {playWithId} = useContext
+  const {playWithId} = useContext(PlayerContext)
 
   return (
     <div className="min-w-[180px] p-2 px-3 rounded cursor-pointer hover:bg-[#ffffff26]">
