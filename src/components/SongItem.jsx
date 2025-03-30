232092bb103f2c365819db8a15bd7ddebfd07cc7
LONTSIPARFAIT <lontsiparfait12@gmail.com>
2025-03-30 16:51:33 +0100

    spotify

--- src/components/SongItem.jsx
+++ src/components/SongItem.jsx
@@ -1,6 +1,9 @@
 import React from "react";
 
 const SongItem = ({ image, name, desc, id }) => {
+
+  const
+
   return (
     <div className="min-w-[180px] p-2 px-3 rounded cursor-pointer hover:bg-[#ffffff26]">
       <img className="rounded" src={image} alt="" />
