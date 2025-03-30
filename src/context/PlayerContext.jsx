f5dcc6198ecca00aa34065ac86691a78d815a10f
LONTSIPARFAIT <lontsiparfait12@gmail.com>
2025-03-30 17:09:38 +0100

    spotify

--- src/context/PlayerContext.jsx
+++ src/context/PlayerContext.jsx
@@ -40,7 +40,7 @@ const PlayerContextProvider = (props) => {
   const previous = async () => {
     if (track.id>0) {
       await setTrack(songsData[track.id-1])
-      await audioRef
+      await audioRef.current.play()
     }
   }
 
