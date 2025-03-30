aa3c6274c07ba25454f9e77fe6bbcce62fc6323b
LONTSIPARFAIT <lontsiparfait12@gmail.com>
2025-03-30 17:31:20 +0100

    spotify

--- src/context/PlayerContext.jsx
+++ src/context/PlayerContext.jsx
@@ -54,7 +54,7 @@ const PlayerContextProvider = (props) => {
   }
 
   const seekSong = async (e) => {
-    audioRef.current.currentTime = ()
+    audioRef.current.currentTime = ((e))
     console.log(e);
     
   } 
