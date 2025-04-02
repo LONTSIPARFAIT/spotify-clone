75ffa0ddfda1e77dad3404a14fd9ade25d6620e9
LONTSIPARFAIT <lontsiparfait12@gmail.com>
2025-04-02 20:28:28 +0100

    spotify

--- src/assets/assets.js
+++ src/assets/assets.js
@@ -197,7 +197,7 @@ export const songsData = [
     name: "7event",
     image: img12,
     file: song11,
-    artist: "S",
+    artist: "Se",
     genre: "Afrobeat",
     desc: "An energetic Afrobeat track",
     duration: "2:35",
