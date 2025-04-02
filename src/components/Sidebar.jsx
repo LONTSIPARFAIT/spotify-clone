6fbc2230bbaede95442e3c0eecc4fd5222eb8160
LONTSIPARFAIT <lontsiparfait12@gmail.com>
2025-04-02 20:40:09 +0100

    spotify

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -37,7 +37,7 @@ const Sidebar = () => {
             </div>
             <div className="p-4 bg-[#242424] m-2 rounded font-semibold flex flex-col items-start justify-start gap-1 pl-4">
                 <h1>Trouvons quelques podcasts à suivre</h1>
-                <p className='font-light'>nous vous tiendrons au courant des nouveaux épisodes</p>
+                <p className='font-light'>Nous vous tiendrons au courant des nouveaux épisodes</p>
                 <button className='px-4 py-1.5 bg-white text-[15px] text-black rounded-full mt-4 cursor-pointer'>Parcourir le podcast</button>
             </div>
         </div>
